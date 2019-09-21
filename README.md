c51ae57a2c06d19a802b81cf116b652b32e43282
sonab <sonabstudios@gmail.com>
2019-09-21 10:15:06 +0800

    updated readme.file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# NodeJS-NotesApp
+# node.js complete
+
+> Basic node.js applications 
+
+## Build Setup
+
+# go to the app directory
+$ cd <directory>
+
+# install dependencies
+$ npm install
+
+# run with node and parameters
+$ node 
--- weather-app/utils/cfg.js
+++ weather-app/utils/cfg.js
@@ -1,6 +1,8 @@
 
-const darkSkyApiKey = 'cb3c22b832d1777c82e6735ae0d9a937'
-const mapBoxToken = 'pk.eyJ1Ijoic29uYWJzdHVkaW9zIiwiYSI6ImNrMHB1a2NocDAxcnczbHJwbDduNXV5azUifQ.jyy16RZsXfuAxy9f4YBGaA'
+// this file shall contain your api keys and tokens
+
+const darkSkyApiKey = ''
+const mapBoxToken = ''
 
 module.exports = {
     darkSkyApiKey,
